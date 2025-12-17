bc9c026e538188d883ec3b7e52a0dc4aea05a918
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2025-12-17 11:10:11 +0100

    1. Spring wiring/DI - exercise

--- e1/rental-store/src/main/java/com/pivovarit/Application.java
+++ e1/rental-store/src/main/java/com/pivovarit/Application.java
@@ -1,6 +1,9 @@
 package com.pivovarit;
 
 // 1. podpiąć springa
+// 2. dependency injection z adnotacjami (RentalService.findAllMovies)
+// 3. dependency injection z Java config (RentalService.findAllMovies)
+// mvn spring-boot:run
 public class Application {
     public static void main(String[] args) {
     }
