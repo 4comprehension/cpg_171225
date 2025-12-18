6f5962b05596dcbdd6ce87e255fd54693b6e32b2
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2025-12-18 09:57:17 +0100

    5. Rest API - exercise

--- e1/rental-store/src/main/java/com/pivovarit/rental/web/RentalController.java
+++ e1/rental-store/src/main/java/com/pivovarit/rental/web/RentalController.java
@@ -3,5 +3,9 @@ package com.pivovarit.rental.web;
 
 // TODO
 public class RentalController {
-
+    // RentalService
+    // GET /movies (zwracanie wszystkich filmów)
+    // GET /movies?type=NEW (filtracja po typie filmu)
+    // GET /movies/{id} (pobieranie filmu po ID)
+    // POST /movies (dodawanie nowego filmu)
 }
