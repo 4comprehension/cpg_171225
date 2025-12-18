698a862c64d020a986faed3ba4c79eca3f658e78
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2025-12-18 13:58:04 +0100

    8. Blackbox tests scaffolding

---
+++ e1/rental-store/src/test/java/com/pivovarit/blackbox/BlackboxTest.java
@@ -0,0 +1,11 @@
+package com.pivovarit.blackbox;
+
+import org.junit.jupiter.api.Test;
+
+class BlackboxTest {
+
+    @Test
+    void example_() throws Exception {
+        System.out.println("foo");
+    }
+}
