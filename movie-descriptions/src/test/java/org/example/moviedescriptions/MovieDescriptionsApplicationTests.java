03d97ee4f0d971ac22b9d9b38ef0080242ea5ae7
artoor-b <artur.brzezina0@gmail.com>
2026-01-02 18:02:52 +0100

    init blank movie-description module

---
+++ movie-descriptions/src/test/java/org/example/moviedescriptions/MovieDescriptionsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.moviedescriptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieDescriptionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
