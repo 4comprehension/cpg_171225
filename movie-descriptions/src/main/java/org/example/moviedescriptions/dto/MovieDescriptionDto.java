97db440a9f6db21f4bd8b4d1c9347ebc7b973206
artoor-b <artur.brzezina0@gmail.com>
2026-01-04 19:16:54 +0100

    simplify mvc

---
+++ movie-descriptions/src/main/java/org/example/moviedescriptions/dto/MovieDescriptionDto.java
@@ -0,0 +1,4 @@
+package org.example.moviedescriptions.dto;
+
+public record MovieDescriptionDto(Long movieId, String description) {
+}
---
+++ movie-descriptions/src/main/java/org/example/moviedescriptions/model/Movie.java
@@ -0,0 +1,40 @@
+package org.example.moviedescriptions.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "movies")
+public class Movie {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long movieId;
+    
+    @Size(max = 2000, message = "Description must not exceed 2000 characters")
+    @Column(length = 2000)
+    private String description;
+    
+    public Movie() {}
+    
+    public Movie(Long movieId, String description) {
+        this.movieId = movieId;
+        this.description = description;
+    }
+    
+    public Long getMovieId() {
+        return movieId;
+    }
+    
+    public void setMovieId(Long movieId) {
+        this.movieId = movieId;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
